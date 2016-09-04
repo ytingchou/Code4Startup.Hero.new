e17bf30638223404bcf4d805c1d55f6d1bb5511f
Yuting Chou <yuting_chou@htc.com>
2016-09-04 17:47:13 +0800

    Set videos path as root path.

--- Code4Newbies/config/routes.rb
+++ Code4Newbies/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  root 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
